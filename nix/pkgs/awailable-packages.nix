c1935c5822e345adc90b991bb232228a1de98748
Marek Fajkus <marek.faj@gmail.com>
2025-11-20 14:11:33 +0100

    generate preinstalled packages

--- nix/pkgs/awailable-packages.nix
+++ nix/pkgs/awailable-packages.nix
@@ -1,15 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}
+, preinstalled-r-packages ? []
+}:
 let
   full-package-list = builtins.attrNames pkgs.rPackages;
   r-packages-all-json = builtins.toFile "r-packages-all.json"
-    (builtins.toJSON flu-package-list);
+    (builtins.toJSON full-package-list);
+
+  r-packages-preinstalled-json = builtins.toFile "r-packages-preinstalled.json"
+    (builtins.toJSON preinstalled-r-packages);
 in
 pkgs.stdenv.mkDerivation {
-  pname = "available-r-packages-json";
+  pname = "r-packages-jsons";
   version = "0.0.1";
   dontUnpack = true;
   installPhase = ''
     mkdir -p $out/var
     cp ${r-packages-all-json} $out/var/r-packages-all.json
+    cp ${r-packages-preinstalled-json} $out/var/r-packages-preinstalled.json
   '';
 }
